1fa5d8d3b2d83a84f5ef29eac69d737b86b5545e
Ivan-Tishchenko <114497011+Ivan-Tishchenko@users.noreply.github.com>
2023-05-21 13:45:31 +0300

    ImegeGalerry was refactoring

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,29 +1,35 @@
-import { Component } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import css from './ImageGallery.module.css';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem.jsx';
 import { Audio } from 'react-loader-spinner';
 import PropTypes from 'prop-types';
 
-export class ImageGallery extends Component {
-  key = '36365507-24ff3c73b2908c2f05119dccb';
+export const ImageGallery = props => {
+  const key = useRef('36365507-24ff3c73b2908c2f05119dccb');
+  const prevProps = useRef({});
 
-  state = {
-    data: [],
-    loading: false,
-  };
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  componentDidUpdate = async (prevProps, prevState) => {
+  useEffect(() => {
+    fetchAPI();
+    prevProps.current = { page: props.page, searchWord: props.searchWord };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.page, props.searchWord]);
+
+  const fetchAPI = async () => {
     if (
-      prevProps.page !== this.props.page ||
-      prevProps.searchWord !== this.props.searchWord
+      prevProps.current.page !== props.page ||
+      prevProps.current.searchWord !== props.searchWord
     ) {
-      this.setState({ loading: true });
+      setLoading(true);
 
-      fetch(
-        `https://pixabay.com/api/?q=${this.props.searchWord}&page=${this.props.page}&key=${this.key}&image_type=photo&orientation=horizontal&per_page=12`
+      await fetch(
+        `https://pixabay.com/api/?q=${props.searchWord}&page=${props.page}&key=${key.current}&image_type=photo&orientation=horizontal&per_page=12`
       )
         .then(response => {
           if (!response.ok) {
+            console.log(response);
             throw new Error(response.status);
           }
           return response.json();
@@ -35,56 +41,50 @@ export class ImageGallery extends Component {
             largeImageURL: obj.largeImageURL,
           }));
         })
-        .then(data => {
-          const formatedData =
-            this.props.page > 1 ? [...this.state.data, ...data] : data;
-          this.setState({ data: formatedData });
+        .then(newData => {
+          const formatedData = props.page > 1 ? [...data, ...newData] : newData;
+          setData(formatedData);
         })
         .catch(error => {
           console.log(error.message);
         })
         .finally(() => {
-          this.setState({
-            loading: false,
-          });
+          setLoading(false);
         });
+      console.log('asdasd');
     }
+    console.log('dsadsa');
   };
 
-  render() {
-    return (
-      <>
-        {this.state.loading && (
-          <div
-            style={{
-              width: '100%',
-              display: 'flex',
-              justifyContent: 'center',
-            }}
-          >
-            <Audio
-              height="280"
-              width="280"
-              radius="29"
-              color="green"
-              ariaLabel="loading"
-              wrapperStyle
-              wrapperClass
-            />
-          </div>
-        )}
-        {!this.state.loading && (
-          <ul className={css.gallery}>
-            <ImageGalleryItem
-              data={this.state.data}
-              scroll={this.props.scroll}
-            />
-          </ul>
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      {loading && (
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <Audio
+            height="280"
+            width="280"
+            radius="29"
+            color="green"
+            ariaLabel="loading"
+            wrapperStyle
+            wrapperClass
+          />
+        </div>
+      )}
+      {!loading && (
+        <ul className={css.gallery}>
+          <ImageGalleryItem data={data} scroll={props.scroll} />
+        </ul>
+      )}
+    </>
+  );
+};
 
 ImageGallery.propTypes = {
   searchWord: PropTypes.string,
